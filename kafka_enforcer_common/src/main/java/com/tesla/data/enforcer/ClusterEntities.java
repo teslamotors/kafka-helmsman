16441e3f8889869a7fe3fc1bdd2e6429e45f17e4
Matteo Baccan <matteo.baccan@gmail.com>
2021-06-30 02:23:24 +0200

    Removed unused imports

--- kafka_enforcer_common/src/main/java/com/tesla/data/enforcer/ClusterEntities.java
+++ kafka_enforcer_common/src/main/java/com/tesla/data/enforcer/ClusterEntities.java
@@ -10,7 +10,6 @@ import tesla.shade.com.google.common.collect.Sets;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
