ccfc2be4660034ac82c33349a52821302e3b9638
Matt Bonnell <matt.bonnell@icloud.com>
2021-09-17 12:07:31 -0400

    Rename variable in Burrow::getClusterBootstrapServers

--- kafka_consumer_freshness_tracker/src/main/java/com/tesla/data/consumer/freshness/Burrow.java
+++ kafka_consumer_freshness_tracker/src/main/java/com/tesla/data/consumer/freshness/Burrow.java
@@ -119,8 +119,8 @@ class Burrow {
   public List<String> getClusterBootstrapServers(String cluster) throws IOException {
     final Map<String, Object> response = this.request(cluster);
     final Map<String, Object> clusterDetail = (Map<String, Object>) response.get("cluster");
-    final Map<String, Object> clusterDetailModuleSection = (Map<String, Object>) clusterDetail.get("module");
-    return (List<String>) clusterDetailModuleSection.get("servers");
+    final Map<String, Object> module = (Map<String, Object>) clusterDetail.get("module");
+    return (List<String>) module.get("servers");
   }
 
   public class ClusterClient {
