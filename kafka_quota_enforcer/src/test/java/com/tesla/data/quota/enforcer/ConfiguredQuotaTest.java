236b8e4446bc2745fde5d8ed2f1c66a237077d62
George Sun <1467726+shk3@users.noreply.github.com>
2025-03-23 17:20:01 +0800

    Add a test case to make sure the double typed quota objects are parsable, including a backward compatibility test.

--- kafka_quota_enforcer/src/test/java/com/tesla/data/quota/enforcer/ConfiguredQuotaTest.java
+++ kafka_quota_enforcer/src/test/java/com/tesla/data/quota/enforcer/ConfiguredQuotaTest.java
@@ -1,9 +1,16 @@
 package com.tesla.data.quota.enforcer;
 
+import com.tesla.data.enforcer.BaseCommand;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 public class ConfiguredQuotaTest {
   @Rule
@@ -25,5 +32,31 @@ public class ConfiguredQuotaTest {
     Assert.assertNotEquals(quota, new ConfiguredQuota("user1", "clientA", 9999d, 1000d, 50d));
     Assert.assertNotEquals(quota, new ConfiguredQuota("user1", "clientA", 2000d, 9999d, 50d));
     Assert.assertNotEquals(quota, new ConfiguredQuota("user1", "clientA", 2000d, 1000d, 99d));
+  }
+
+  @Test
+  public void testConverter() throws Exception {
+    String cfg = String.join("\n",
+        "---",
+        "kafka:",
+        "    bootstrap.servers: localhost:9092",
+        "quota:",
+        "    - principal: user1",
+        "      client: float-client",
+        "      producer-byte-rate: 2000.0",
+        "      consumer-byte-rate: 1000.0",
+        "      request-percentage: 52.7",
+        "    - principal: backward-compatible",
+        "      client: all-integers",
+        "      producer-byte-rate: 2000",
+        "      consumer-byte-rate: 1000",
+        "      request-percentage: 50");
+    final BaseCommand.CommandConfigConverter converter = new BaseCommand.CommandConfigConverter();
+    Map<String, Object> config = converter.convert(new ByteArrayInputStream(cfg.getBytes(StandardCharsets.UTF_8)));
+    List<ConfiguredQuota> quotas =
+        new BaseCommand<ConfiguredQuota>(config).configuredEntities(ConfiguredQuota.class, "quota", "quotaFile");
+    Assert.assertEquals(quotas, List.of(
+        new ConfiguredQuota("user1", "float-client", 2000d, 1000d, 52.7),
+        new ConfiguredQuota("backward-compatible", "all-integers", 2000d, 1000d, 50d)));
   }
 }
